9e7e3946aa51d7ec1914e474d35901b89a10ec91
janet-do <124389477+janet-do@users.noreply.github.com>
2023-04-14 03:47:18 +0000

    sync

---
+++ go_app/main.go
@@ -0,0 +1,30 @@
+// Created by: Janet Do
+// Created on: Sep 2020
+//
+// This program shows conversion from Fahrenheit to Celsius
+
+package main
+
+import "fmt"
+
+func main() {
+	// This function shows conversion from Fahrenheit to Celsius
+
+	var fahrenheit int
+	var celsius int
+
+	// input
+	fmt.Println("This program shows conversion from Fahrenheit to Celsius")
+	fmt.Println()
+	fmt.Print("Enter Fahrenheit: ")
+	fmt.Scanln(&fahrenheit)
+
+	// process
+	celsius = (fahrenheit - 32) * 5/9
+
+	// output
+	fmt.Println()
+	fmt.Println("converted to celsius is:", celsius, "℃.")
+	fmt.Println()
+	fmt.Println("\nDone.")
+}
